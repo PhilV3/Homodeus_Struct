be2009c37a807dc401ea0447d27c9ac152ecbcb7
PhilV3 <debp1101@usherbrooke.ca>
2024-09-05 12:26:36 -0400

    Test du push

---
+++ HomoDeus/launch_all_nodes.bash
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+homodeus_home=${PWD}
+
+filter_node_name="Filters"
+filter_node="rosrun hbba_state filter_node.py"
+
+hbba_lite_main_node_name="HBBA"
+hbba_lite_main_node="rosrun hbba_state hbba_state_node"
+
+nav_node_name="Navigation"
+nav_node="rosrun NavigationSelector main_navSelector.py"
+
+#pseudo_detection_node_name="PseudoDetection"
+#pseudo_detection_node="rosrun pseudo_detection pseudo_detection" 
+
+hd_audio_node_name="Audio"
+hd_audio_node="rosrun HD_audio talkInterface.py"
+
+hd_vision_node_name="Vision"
+hd_vision_node="python pseudo_detection/scripts/object_detection_package/detect.py" 
+eval "source ../../devel/setup.bash"
+eval "rosmaster"
+eval "rosip"
+
+# Commands to run in each terminal window
+
+gnome-terminal \
+  --tab --title=${filter_node_name} -e "bash -c '$filter_node; exec bash'" \
+  --tab --title=${nav_node_name}  -e "bash -c '$nav_node; exec bash'" \
+  --tab --title=${hd_audio_node_name}  -e "bash -c '$hd_audio_node; exec bash'" \
+  --tab --title=${hbba_lite_main_node_name} -e "bash -c '$hbba_lite_main_node; exec bash'" \
+  #--tab --title=${hd_vision_node_name}  -e "bash -c '$hd_vision_node; exec bash'" \
+  #--tab --title=${pseudo_detection_node_name} -e "bash -c '$pseudo_detection_node; exec bash'" \
+
+
